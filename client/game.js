93fe850b0b5d0e49da7b9a0218c2fe0ebd65874d
sonnybardhan <sonnybardhan@github.io>
2020-06-16 17:48:32 +0530

    develop join method on client and server

--- client/game.js
+++ client/game.js
@@ -1,20 +1,83 @@
 let HOST = 'ws://localhost:9090';
-const ws = new WebSocket(HOST);
+export const ws = new WebSocket(HOST);
+const nameInput = document.getElementById('name-input');
+const gameIdInput = document.getElementById('game-id-input');
+const createBtn = document.getElementById('create-btn');
+const joinBtn = document.getElementById('join-btn');
+const spaceBar = document.getElementById('space-bar');
+
+let clientId = null;
+let gameId = null;
+let playerName = null;
+let playerNum = null;
 
 ws.onopen = (e) => console.log('connected to server');
+
+ws.onmessage = (msg) => {
+	const response = JSON.parse(msg.data);
+	// console.log(response);
+
+	if (response.method === 'connect') {
+		clientId = response.clientId;
+		console.log(`client ${clientId} successfully added`);
+	} else if (response.method === 'create') {
+		console.log('server sent a message: ', response);
+		gameId = response.game.id;
+		gameIdInput.value = gameId;
+		//this should implicitly 'join' the creator
+		//generate a time out
+	} else if (response.method === 'join') {
+		console.log('server sent a message: ', response);
+	} else if (response.method === 'play') {
+	} else if (response.method === 'error') {
+		console.log('There was an error!', response.msg);
+	}
+};
+
+createBtn.addEventListener('click', () => {
+	const payload = {
+		method: 'create',
+		clientId
+	};
+	ws.send(JSON.stringify(payload));
+});
+
+joinBtn.addEventListener('click', () => {
+	gameId = gameIdInput.value;
+	playerName = nameInput.value;
+
+	if (!gameId) {
+		return console.log('Please enter a game ID');
+	}
+
+	const payload = {
+		method: 'join',
+		clientId,
+		gameId,
+		playerName
+	};
+	ws.send(JSON.stringify(payload));
+});
+
 ws.onclose = (e) => console.log('disconnected from server');
 ws.onerror = (e) => console.log('Oop! ', e);
+
+//game logic=====================
 
 import { SPEED, update as updateSnake, draw as drawSnake } from './snake.js';
 
 import { update as updateFood, draw as drawFood } from './food.js';
 
-// export var numPlayers = 2;
+//elements from the start screen
+
+//ignore this during development
+export const overlay = document.getElementById('overlay');
 
 let paused = true;
+
+//in game elements
 const gameBoard = document.getElementById('game-board');
-export const overlay = document.getElementById('overlay');
-// export const crashScreen = document.getElementById('crash-screen');
+
 let lastRender = 0;
 
 function main(currentTime) {
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//SNAKE GAME==============
+
 // const PORT = process.env.PORT || 9090;
 const PORT = 9090;
 const express = require('express');
@@ -17,7 +19,7 @@ wss.on('request', (req) => {
 	const connection = req.accept(null, req.origin);
 
 	console.log('client made a request ... ');
-
+	//on client's initial request
 	const clientId = 'PLAYER-' + createId();
 	clients[clientId] = { connection };
 	const payload = {
@@ -27,7 +29,98 @@ wss.on('request', (req) => {
 
 	connection.send(JSON.stringify(payload));
 
-	connection.on('close', () => console.log(`client: ${clientId} disconnected.`));
+	connection.on('message', (msg) => {
+		const response = JSON.parse(msg.utf8Data);
+
+		if (response.method === 'create') {
+			const clientId = response.clientId;
+			const gameId = 'GAME-' + createId();
+			console.log(`client ${clientId} wants to create a new game`);
+
+			games[gameId] = {
+				id: gameId,
+				clients: [],
+				state: {}
+			};
+
+			// games[gameId].clients.push(clientId);
+
+			const payload = {
+				method: 'create',
+				game: games[gameId]
+			};
+			const con = clients[clientId].connection;
+			con.send(JSON.stringify(payload));
+
+			// for (let [ i, client ] of Object.keys(clients).entries()) {
+			// 	console.log(i, client);
+			// }
+		} else if (response.method === 'join') {
+			//should have an client ID
+			const clientId = response.clientId;
+			//should have game ID and error handling
+			const gameId = response.gameId;
+			let playerName = response.playerName;
+			let playerIndex = null;
+			const game = games[gameId];
+			const con = clients[clientId].connection;
+			// con.send(JSON.stringify(payload));
+
+			//check for valid id
+			if (!game) {
+				const payload = {
+					method: 'error',
+					msg: 'Invalid game ID'
+				};
+				return con.send(JSON.stringify(payload));
+			}
+
+			//check for number of players and start accordingly
+			if (game.clients.length >= 2) {
+				const payload = {
+					method: 'error',
+					msg: 'Sorry max players reached'
+				};
+				return con.send(JSON.stringify(payload));
+			}
+
+			//check if player is already in game
+			playerIndex = game.clients.indexOf(clientId);
+
+			if (playerIndex > -1) {
+				const payload = {
+					method: 'error',
+					msg: 'Invalid request, already in game.'
+				};
+				return con.send(JSON.stringify(payload));
+			}
+
+			if (!playerName) {
+				playerName = 'Player-' + (playerIndex + 1);
+			}
+
+			game.clients.push({
+				clientId,
+				playerName
+			});
+
+			const payload = {
+				method: 'join',
+				game
+			};
+
+			game.clients.forEach((client) => {
+				clients[client.clientId].connection.send(JSON.stringify(payload));
+			});
+		}
+	});
+
+	connection.on('close', () => {
+		//remove client from game and update
+		//store clientID and game ID together in the clients object
+		// clients[clientId] = {connection, gameId}
+		console.log(`client: ${clientId} disconnected.`);
+	});
 });
 
 function createId(len = 6, chars = 'abcdefghijklmnopqrstuvxyz0123456789') {
